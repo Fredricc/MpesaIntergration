a9c7ee86c44eefe0d9977b4d7fb68f43a8e34fc7
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-07-19 22:58:36 +0300

    Token

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using MpesaIntergration.Data;
 using MpesaIntergration.Models;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net.Mime;
+using System.Text;
 
 namespace MpesaIntergration.Controllers
 {
@@ -10,10 +13,13 @@ namespace MpesaIntergration.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IHttpClientFactory _clientFactory;
 
-        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
+        private ApplicationDbContext _dbContext;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory, ApplicationDbContext dbContext)
         {
             _logger = logger;
             _clientFactory = clientFactory;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
@@ -23,7 +29,7 @@ namespace MpesaIntergration.Controllers
 
         public async Task<string> GetToken()
         {
-            var client = _clientFactory.CreateClient("mpesa");
+            var client = _clientFactory.CreateClient("mpesa"); 
             var authString = "khhvbjI2OD0FP2CWnMtqZ6GmYkyO2rylGj3kqFnTvmIaGEJ3 : 91CTcgoq8aKGYwgQAiibVDD8SQWoyMGrj3LoFM1AH3raiMxQXAwArj5asAhXnB6u";
 
             var encodedString = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authString));
@@ -40,7 +46,8 @@ namespace MpesaIntergration.Controllers
 
             Token tokenObject = JsonConvert.DeserializeObject<Token>(mpesaResponse);
 
-            return tokenObject.access_token;
+            return mpesaResponse;
+            //return tokenObject.access_token;
             
         }
 
@@ -50,6 +57,77 @@ namespace MpesaIntergration.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        [Route("register-urls")]
+        public async Task<string> RegisterMpesaUrls()
+        {
+            var jsonBody = JsonConvert.SerializeObject(new
+            {
+                ValidationURL = "https://c3f1-102-135-169-122.eu.ngrok.io/payments/confirmation",
+                ConfirmationURL = "https://c3f1-102-135-169-122.eu.ngrok.io/payments/validation",
+                ResponseType = "Completed",
+                ShortCode = 600990
+            });
+
+            var jsonReadyBody = new StringContent(
+                jsonBody.ToString(),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var token = await GetToken();
+
+            var client = _clientFactory.CreateClient("mpesa");
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            var url = "/mpesa/c2b/v1/registerurl";
+
+            var response = await client.PostAsync(url, jsonReadyBody);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        // Confirmation endpoint
+        [HttpPost]
+        [Route("payments/confirmation")]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<JsonResult> PaymentConfirmation([FromBody] MpesaC2B c2bPayments)
+        {
+            var respond = new
+            {
+                ResponseCode = 0,
+                ResponseDesc = "Processed"
+            };
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(c2bPayments);
+                var saveResponse = await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                return Json(new { code = 0, errors = ModelState });
+            }
+
+            return Json(respond);
+
+        }
+
+        [HttpPost]
+        [Route("payments/validation")]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<JsonResult> PaymentValidation([FromBody] MpesaC2B c2bPayments)
+        {
+            var respond = new
+            {
+                ResponseCode = 0,
+                ResponseDesc = "Processed"
+            };
+            return Json(respond);
+        }
+
 
         public IActionResult Privacy()
         {
--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
+using MpesaIntergration.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddHttpClient("mpesa", C =>
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// http client
+builder.Services.AddHttpClient("mpesa", c =>
 {
-    C.BaseAddress = new Uri("https://sandbox.safaricom.co.ke");
-});
+    c.BaseAddress = new Uri("https://sandbox.safaricom.co.ke");
+    //  c.DefaultRequestHeaders.Add("Content-Type", "application/json");
+}
+);
+
+//var serverVersion = new MySqlServerVersion(new Version(9, 0, 0));
+//// database context
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//options
+//.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), serverVersion)
+//);
+
+
 
 var app = builder.Build();
 
